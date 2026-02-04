c3b2171ec0831f7e9330dd4659be964b5dd263da
David Pall <davidpallatx@gmail.com>
2026-02-04 10:45:31 +0100

    - minor fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://davidpall.github.io",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
